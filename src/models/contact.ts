b49ac4a22fdb1f4b09fc814d365ba46d2eff5b03
AlxBDo <alexbidaud85@gmail.com>
2025-07-26 19:32:44 +0200

    repo cleaning

--- src/models/contact.ts
+++
@@ -1,7 +0,0 @@
-import type { Item } from "./item";
-
-export interface Contact extends Item {
-    firstname?: string;
-    email?: string;
-    lastname?: string;
-}
--- src/models/user.ts
+++
@@ -1,6 +0,0 @@
-import type { Contact } from "./contact";
-
-export interface User extends Contact {
-  lists?: any;
-  password?: string;
-}
